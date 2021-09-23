bbd82af253bdc92dfd5a9a8cf53206739096570d
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2021-09-23 01:08:22 -0700

    fix deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,8 @@
 #lang info
 (define collection "fmt")
-(define deps '("base"))
+(define deps '("pprint-compact"
+               "syntax-color-lib"
+               "base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/fmt.scrbl" ())))
 (define pkg-desc "Description Here")
