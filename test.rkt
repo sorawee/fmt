c708ab302d4676dbe733ccbb4959e02c9153e511
Sorawee Porncharoenwase <soraweep@amazon.com>
2022-06-29 17:25:55 -0700

    track more information to see what goes wrong

--- test.rkt
+++ test.rkt
@@ -6,7 +6,10 @@
            rackunit)
 
   (define-runtime-path test-hash-bang.rkt "tests/test-hash-bang.rkt")
+  (check member 'execute (file-or-directory-permissions test-hash-bang.rkt)
+         "before -i")
   (define raco-fmt (hash-ref (all-tools) "fmt"))
   (parameterize ([current-command-line-arguments (vector "-i" (~a test-hash-bang.rkt))])
     (dynamic-require (second raco-fmt) #f))
-  (check member 'execute (file-or-directory-permissions test-hash-bang.rkt)))
+  (check member 'execute (file-or-directory-permissions test-hash-bang.rkt)
+         "after -i"))
