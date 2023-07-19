f1f1d1000cc3c16a3c5182d5f31cd9080d30f597
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2023-07-18 23:57:06 -0700

    test: correct the consistency check

--- regen.rkt
+++ regen.rkt
@@ -2,17 +2,26 @@
 
 #lang racket
 
-(require file/glob)
+(require file/glob
+         raco/all-tools)
 
 (define paths '("tests/test-cases/*.rkt"
                 "tests/benchmarks/*.rkt"))
 
 (define out-ext "out")
 
+(define raco-fmt (hash-ref (all-tools) "fmt"))
+
 (define (regen ext)
-  (for ([f (flatten (map glob paths))])
+  (for ([f (flatten (map glob paths))] [i (in-naturals)])
     (printf "formatting ~a\n" f)
-    (system (format "raco fmt ~a > ~a.~a" f f ext))))
+
+    (with-output-to-file (format "~a.~a" f ext)
+      #:exists 'replace
+      (λ ()
+        (parameterize ([current-command-line-arguments (vector (~a f))]
+                       [current-namespace (make-base-namespace)])
+          (dynamic-require (second raco-fmt) #f))))))
 
 (module+ main
   (regen out-ext))
@@ -20,13 +29,11 @@
 (module+ test
   (require rackunit)
 
-  (define-check (check-file f)
-    (unless (equal? (file->string (format "~a.~a" f check-ext))
-                    (file->string (format "~a.~a" f out-ext)))
-      (fail-check (format "Found a mismatch on '~a'" f))))
-
   (define check-ext "out-check")
   (regen check-ext)
   (for ([f (flatten (map glob paths))])
-    (check-file f)
+    (with-check-info (['filename f])
+      (check-equal? (file->string (format "~a.~a" f check-ext))
+                    (file->string (format "~a.~a" f out-ext))))
+
     (delete-file (format "~a.~a" f check-ext))))
